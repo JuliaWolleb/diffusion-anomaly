c414e0eb29dc287db7692da07305a190d6af39c4
JuliaWolleb <67380907+JuliaWolleb@users.noreply.github.com>
2022-08-31 11:32:45 +0200

    Update train_util.py
    change visdom port

--- guided_diffusion/train_util.py
+++ guided_diffusion/train_util.py
@@ -13,11 +13,7 @@ from .fp16_util import MixedPrecisionTrainer
 from .nn import update_ema
 from .resample import LossAwareSampler, UniformSampler
 from visdom import Visdom
-viz = Visdom(port=8851)
-loss_window = viz.line( Y=th.zeros((1)).cpu(), X=th.zeros((1)).cpu(), opts=dict(xlabel='epoch', ylabel='Loss', title='loss'))
-grad_window = viz.line(Y=th.zeros((1)).cpu(), X=th.zeros((1)).cpu(),
-                           opts=dict(xlabel='step', ylabel='amplitude', title='gradient'))
-
+viz = Visdom(port=8850)
 
 INITIAL_LOG_LOSS_SCALE = 20.0
 
